7d1f17b5d26c99cd3553527b647a0a662a37148f
Abhinay <Abhinay>
2023-10-05 00:57:38 +0530

    Converters and added database

---
+++ app/src/main/java/com/androiddevs/runningappyt/db/RunningDatabase.kt
@@ -0,0 +1,19 @@
+package com.androiddevs.runningappyt.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+/**
+ * Created by Abhinay on 05/10/23.
+ */
+@Database(
+      entities = [Run::class],
+      version = 1
+)
+@TypeConverters(Converters::class)
+abstract  class RunningDatabase : RoomDatabase() {
+
+    abstract fun getRunDao(): RunDAO
+
+}
