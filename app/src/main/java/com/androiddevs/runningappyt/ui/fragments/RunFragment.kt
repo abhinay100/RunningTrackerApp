284b6913ce4c369aa0de9aabcb6450719dc96c93
Abhinay <Abhinay>
2023-10-05 21:19:01 +0530

    Setting Up Navigation

--- app/src/main/java/com/androiddevs/runningappyt/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/RunFragment.kt
@@ -1,10 +1,14 @@
 package com.androiddevs.runningappyt.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 /**
  * Created by Abhinay on 05/10/23.
@@ -14,4 +18,12 @@ class RunFragment : Fragment(R.layout.fragment_run) {
 
     //Dagger hilt will setup viewmodel factory by default by using viewmodels
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
