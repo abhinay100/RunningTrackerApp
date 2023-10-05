0a2e90f7846e15e60d97a2b50e4480f7eb7fa4b1
Abhinay <Abhinay>
2023-10-05 23:19:01 +0530

    Setting up mapview

--- app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,14 @@
 package com.androiddevs.runningappyt.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 /**
  * Created by Abhinay on 05/10/23.
@@ -13,4 +17,45 @@ import dagger.hilt.android.AndroidEntryPoint
 class TrackingFragment: Fragment(R.layout.fragment_tracking) {
     //Dagger hilt will setup viewmodel factory by default by using viewmodels
     private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
 }
