ce23f6046f2a8abd1cc402f6a1433059596729d1
Abhinay <Abhinay>
2023-10-09 00:00:55 +0530

    Saving Run in Db

--- app/src/main/java/com/androiddevs/runningappyt/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/viewmodels/MainViewModel.kt
@@ -2,7 +2,10 @@ package com.androiddevs.runningappyt.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.runningappyt.db.Run
 import com.androiddevs.runningappyt.repositories.MainRepository
+import kotlinx.coroutines.launch
 
 /**
  * Created by Abhinay on 05/10/23.
@@ -11,4 +14,8 @@ import com.androiddevs.runningappyt.repositories.MainRepository
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
